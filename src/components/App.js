3f99453b37b31937aaffb47e4dac8f22d2c66519
nivas ka <nivas619ka@gmail.com>
2020-08-19 00:27:15 +0200

    added main window

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
 import MainWindow from "./MainWindow";
+import "./App.css";
+
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>App</h1>
         <MainWindow />
       </div>
     );
--- src/components/MainWindow.js
+++ src/components/MainWindow.js
@@ -2,6 +2,32 @@ import React, { Component } from "react";
 
 class MainWindow extends Component {
   render() {
-    return <h1>Main Window</h1>;
+    return (
+      <div className="container-main theme-dark">
+        <div className="menu-bar-top">
+          <div className="toolbar-menu-list">
+            <ul>
+              <li>File</li>
+              <li>Edit</li>
+              <li>View</li>
+              <li>Run</li>
+              <li>Terminal</li>
+              <li>Help</li>
+            </ul>
+          </div>
+          <div class="toolbar-title">Title</div>
+          <div className="toolbar-icons">
+            <ul>
+              <li>Minimise</li>
+              <li>Maximise</li>
+              <li>Close</li>
+            </ul>
+          </div>
+        </div>
+        <div className="container-editor">eitor area</div>
+      </div>
+    );
   }
 }
+
+export default MainWindow;
