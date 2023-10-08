d9e67c98041b4dfd3eb704406b6ec510d145d736
nivas619ka <nivas619ka@gmail.com>
2023-10-08 12:22:21 +0200

    update text content

--- src/textContent.js
+++ src/textContent.js
@@ -1,23 +1,29 @@
 export const skills = `
 <span class="editor-keyword-1">export</span> <span class="editor-keyword-1">default</span> {
-    <span class="editor-text-label">'.Net Framework'</span>: [<span class="editor-text-value">'C#', 'WPF', 'Windows Forms','ASP.Net MVC'</span>],
-    <span class="editor-text-label">'Web Development'</span>: [<span class="editor-text-value">'HTML', 'Javascript', 'CSS', 'SQL', 'React', 'Redux'</span>],
+    <span class="editor-text-label">'.Net Framework'</span>: [<span class="editor-text-value">'C#', 'ASP.Net Core', 'EF Core', 'MS SQL Server', 'WPF', 'Windows Forms'</span>],
+    <span class="editor-text-label">'Web Development'</span>: [<span class="editor-text-value">'HTML', 'TypeScript', 'SASS', 'T-SQL', 'Angular'</span>],
+    <span class="editor-text-label">'Cloud'</span>: [<span class="editor-text-value">'Serverless Functions', 'Service Bus', 'Storage Accounts'</span>],
     <span class="editor-text-label">'SharePoint'</span>: [<span class="editor-text-value">'SharePoint 2013', 'SharePoint Online', 'Sharepoint Designer', 'Nintex Workflows'</span>],
     <span class="editor-text-label">'Others'</span>: [<span class="editor-text-value">'MATLAB', 'Microsoft Power BI'</span>],
     <span class="editor-text-label">'Version Control'</span>: [<span class="editor-text-value">'Git', 'Microsoft TFS'</span>]
 }`;
 
 export const aboutMe = `
-Hey!
+Hey there! 
 
-This is Nivas. I am a software developer from Hamburg.
-I work on .Net framework and Microsoft SharePoint.
+I'm a full stack developer with over five years of experience in the .NET world. 
+I've worked extensively with C#, JavaScript, ASP.NET and Angular. 
 
-I am also very interested in web development and I like programming in JavaScript and C#.
+I am based in Hamburg and looking to relocate back to India in 2024.
+Currently seeking new job opportunities in Bangalore/Pune and Hyderabad.
+If you're interested in discussing how my skills and experience can benefit your team, feel free to reach out to me.
 
 <div contentEditable="false">
 Check out my CV <a href="https://drive.google.com/file/d/1uy4w4IIiSy-KHiGdHNLO648fHf4ZtvUA/view?usp=sharing" target="_blank">here</a>
-Mail me at <a href="mailto:nivas619ka@gmail.com">nivas619ka@gmail.com</a>
+
+Email: <a href="mailto:nivas.anand@outlook.com">nivas.anand@outlook.com</a>
+Phone: <a href="tel:+4917630103096">+49 176 3010 3096</a>
+Whatsapp: <a href="https://wa.me/918884056527" target="_blank">+91 8884056527</a>
 </div>
 `;
 
@@ -33,8 +39,9 @@ with configurable commands
 export const contact = `
 <div contentEditable="false">
 {
-    <span class="editor-text-label">email</span>: <span class="editor-text-value"><a href="mailto:nivas619ka@gmail.com" target="_blank">"nivas619ka@gmail.com"</a></span>,
-    <span class="editor-text-label">phone</span>: <span class="editor-text-value">"+49 176 3010 3096"</span>,
+    <span class="editor-text-label">email</span>: <span class="editor-text-value"><a href="mailto:nivas.anand@outlook.com" target="_blank">"nivas.anand@outlook.com"</a></span>,
+    <span class="editor-text-label">phone-de</span>: <span class="editor-text-value">"+49 17630103096"</span>,
+    <span class="editor-text-label">phone-in</span>: <span class="editor-text-value">"+91 8884056527"</span>,
     <span class="editor-text-label">linkedin</span>: <span class="editor-text-value"><a href="https://www.linkedin.com/in/nivas-k-22152b79/" target="_blank">"https://www.linkedin.com/in/nivas-k-22152b79/"</a></span>,
     <span class="editor-text-label">xing</span>: <span class="editor-text-value"><a href="https://xing.com/profile/nivas_anand" target="_blank">"https://xing.com/profile/nivas_anand"</a></span>
 }
@@ -42,9 +49,10 @@ export const contact = `
 `;
 
 export const readMe = `
-I built this site with React using Redux for state management.
-The design imitates VS Code editor. I used icons from font-awesome.
+I built this site in 2020 when I was learning React and Redux. 
+Wish I had used TypsScript too :/
 
+The design imitates my favourite code editor "Visual Studio Code". I used icons from font-awesome.
 <div contentEditable="false">
 Check out the source code <a href="https://github.com/nivaska/portfolio" target="_blank">here</a>
 </div>
